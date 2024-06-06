ed98a57a158b828136528a814261e5f693368d1e
rlim <remy.lim@universite-paris-saclay.fr>
2024-06-06 16:55:24 +0200

    3e test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rlim.github.io',
+    site: 'https://rlim-git.github.io',
 });
